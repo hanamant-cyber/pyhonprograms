4456ee3ff0e86f04f9e9ae39f4a25fb679789c7d
Paul Philips <paulphilips@email.com>
2025-09-07 14:40:35 +0530

    Added python pograms

--- object.py
+++ object.py
@@ -178,19 +178,20 @@
 
 
     #property method
-class Student:
-    def __init__(self, phy, chem, math):
-        self.phy = phy
-        self.chem = chem
-        self.math = math
+# class Student:
+#     def __init__(self, phy, chem, math):
+#         self.phy = phy
+#         self.chem = chem
+#         self.math = math
 
-    @property
-    def percentage(self):
-        return str((self.phy + self.chem + self.math) / 3) + "%"
+#     @property
+#     def percentage(self):
+#         return str((self.phy + self.chem + self.math) / 3) + "%"
 
 
-stu1 = Student(98, 97, 99)
-print(stu1.percentage)
+# stu1 = Student(98, 97, 99)
+# print(stu1.percentage)
 
-stu1.phy = 77
-print(stu1.percentage)
+# stu1.phy = 77
+# print(stu1.percentage)
+print("hello world")
